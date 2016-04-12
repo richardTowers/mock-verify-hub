ec8737dcedd9932bfb94fedc3a264030cdc32bb8
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-04-12 18:39:21 +0100

    Add cookies to `/api/session/` response
    This allows you to get to the start page!

--- mock-verify-hub.rb
+++ mock-verify-hub.rb
@@ -6,6 +6,7 @@ set :port, 50190
 
 post '/api/session' do
   status 201
-  '{}'
+  session_start_time = (Time.now.to_i * 1000).to_s
+  '{"x_govuk_session_cookie":"","x-govuk-secure-cookie":"","session_start_time":' + session_start_time + '}'
 end
 
