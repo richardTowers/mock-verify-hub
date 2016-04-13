acc4fba2bcbaccb52ff3156972a504828b4d71a3
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-04-13 16:39:01 +0100

    Tidy up code
    No need to use heredocs, can use `%()` instead (thanks ruby koans).
    No need to use Time.now, I've been to the year 3000 and not much has
    changed.

--- mock-verify-hub.rb
+++ mock-verify-hub.rb
@@ -6,58 +6,47 @@ set :port, 50190
 
 post '/api/session' do
   status 201
-  session_start_time = (Time.now.to_i * 1000).to_s
-  <<-eos
-  {
+  %({
     "x_govuk_session_cookie":"",
     "x-govuk-secure-cookie":"",
-    "session_start_time":#{session_start_time}
-  }
-  eos
+    "session_start_time":32503680000000
+  })
 end
 
 get '/api/session/federation' do
-  <<-eos
-  {
+  %({
     "transactionSimpleId":"test-rp",
     "transactionEntityId":"http://example.com/test-rp",
     "idps":[{
       "simpleId":"stub-idp-one",
       "entityId":"http://example.com/stub-idp-one"
     }]
-  }
-  eos
+  })
 end
 
 put '/api/session/select-idp' do
-  <<-eos
-  {
+  %({
     "encryptedEntityId":"not-blank"
-  }
-  eos
+  })
 end
 
 get '/api/session/idp-authn-request' do
-  <<-eos
-  {
+  %({
     "location":"http://www.example.com",
     "samlRequest":"blah",
     "relayState":"whatever",
     "registration":false
-  }
-  eos
+  })
 end
 
 get '/api/transactions' do
-  <<-eos
-  {
+  %({
     "public":[{
       "simpleId":"test-rp",
       "entityId":"http://example.com/test-rp",
       "homepage":"http://example.com/test-rp"
     }],
     "private":[]
-  }
-  eos
+  })
 end
 
