55e749a81f39ba2833e88a88f2e80d70408cd3e5
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2016-04-13 14:37:48 +0100

    Use heredocs to avoid issues with escaping quotes
    It's nice to be able to interpolate the strings, but using double quotes
    makes JSON a pain. Heredocs for the win.

--- mock-verify-hub.rb
+++ mock-verify-hub.rb
@@ -7,10 +7,22 @@ set :port, 50190
 post '/api/session' do
   status 201
   session_start_time = (Time.now.to_i * 1000).to_s
-  '{"x_govuk_session_cookie":"","x-govuk-secure-cookie":"","session_start_time":' + session_start_time + '}'
+  <<-eos
+  {
+    "x_govuk_session_cookie":"",
+    "x-govuk-secure-cookie":"",
+    "session_start_time":#{session_start_time}
+  }
+  eos
 end
 
 get '/api/session/federation' do
-  '{"transactionSimpleId":"test-rp","transactionEntityId":"http://example.com/test-rp","idps":["stub-idp-one"]}'
+  <<-eos
+  {
+    "transactionSimpleId":"test-rp",
+    "transactionEntityId":"http://example.com/test-rp",
+    "idps":["stub-idp-one"]
+  }
+  eos
 end
 
